6fbdf46ac52b5fac896ee8a13c741ff6f19b5a6a
Mabutho83 <mabuthosiyanda83@gmail.com>
2026-09-13 12:41:55 +0200

    Add project files.

---
+++ StayBnB Web Application v2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StayBnB_Web_Application_v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
