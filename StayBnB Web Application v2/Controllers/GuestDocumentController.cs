db008f4e65a616acf7b0528ccfe794f14fcd7485
Mabutho83 <mabuthosiyanda83@gmail.com>
2026-09-23 10:24:56 +0200

    Added controllers for models that didn't have scaffolded controllers.

---
+++ StayBnB Web Application v2/Controllers/GuestDocumentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StayBnB_Web_Application_v2.Controllers
+{
+    public class GuestDocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StayBnB Web Application v2/Controllers/MessagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StayBnB_Web_Application_v2.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
